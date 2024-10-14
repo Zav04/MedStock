58263d4fa1a6c670d81ca3934b42dd31360fe7ae
Bruno Oliveira <bruno.bx04@gmail.com>
2024-10-14 01:19:56 +0100

    [DEVELOP] - View Password Pressing a Button - Windows App Move by app bar working, missing resize

--- Code/MEDSTOCK/Pages/Login/config_login.py
+++ Code/MEDSTOCK/Pages/Login/config_login.py
@@ -1,25 +1,47 @@
 import flet as ft
-
+import ctypes
 
 def config_login_page(page):
     from Pages import maximize_window, minimize_window
-    page.theme_mode = ft.ThemeMode.SYSTEM
+    page.theme_mode = ft.ThemeMode.LIGHT
     
     if page.web:
         page.title = "Login"
-        page.icon = "assets/icon.png"
     elif page.platform.value == "windows":
         page.window.resizable = True
         page.window.full_screen = False
-        page.window.icon = "assets/Icons/Logo.ico"
-        page.window.frameless = True
-        page.appbar = ft.AppBar(
-            title=ft.Text("MedStock"),
-            leading=ft.Image(src="assets/Logo/Lateral/ICO/Logo.ico"),
-            bgcolor=ft.colors.SURFACE_VARIANT,
-            actions=[
-                ft.IconButton(icon=ft.icons.MINIMIZE,on_click=lambda _:minimize_window(page)),
-                ft.IconButton(icon=ft.icons.MAXIMIZE,on_click=lambda _:maximize_window(page)),
-                ft.IconButton(icon=ft.icons.CLOSE, on_click=lambda _:page.window.close()),
-            ],
+        page.window_frameless = True
+
+        page.add(
+            ft.ResponsiveRow([
+                ft.WindowDragArea(
+                    ft.Container(
+                        bgcolor=ft.colors.SURFACE_VARIANT,
+                        padding=0,
+                        content=ft.Row([
+                            ft.Row([
+                                ft.Image(src="assets/favicon.png", width=50, height=50),
+                                ft.Text("MedStock", size=30),
+                            ]),
+                            ft.Row(
+                                controls=[
+                                    ft.IconButton(icon=ft.icons.MINIMIZE_OUTLINED, on_click=lambda _: minimize_window(page)),
+                                    ft.IconButton(icon=ft.icons.CHECK_BOX_OUTLINE_BLANK, on_click=lambda _: maximize_window(page)),
+                                    ft.IconButton(icon=ft.icons.CLOSE_OUTLINED, on_click=lambda _: page.window.close()),
+                                ],
+                                spacing=2
+                            ),
+                        ],
+                        alignment=ft.MainAxisAlignment.SPACE_BETWEEN 
+                        ),
+                    ),
+                )
+            ])
         )
+
+
+def toggle_password_visibility(password_input, icon_button):
+    password_input.password = not password_input.password
+    icon_button.icon = ft.icons.VISIBILITY_OFF if password_input.password else ft.icons.VISIBILITY
+    password_input.update()
+    icon_button.update()
--- Code/MEDSTOCK/Pages/Login/ui_login.py
+++ Code/MEDSTOCK/Pages/Login/ui_login.py
@@ -1,23 +1,69 @@
 import flet as ft
+from flet import *
 from .back_login import login_button_clicked, reset_password_button_clicked, register_button_clicked
-from .config_login import config_login_page
+from .config_login import config_login_page, toggle_password_visibility
 
 def login_page(page: ft.Page):
     
     config_login_page(page)
     
+    # Campo de email
+    email_input = ft.TextField(
+        label="Email", 
+        width=500,
+        height=50,
+        text_align="start"
+    )
     
-    email_input = ft.TextField(label="Email", width=300)
-    password_input = ft.TextField(label="Senha", password=True, width=300)
-    login_button = ft.ElevatedButton(text="Login", on_click=lambda _:login_button_clicked(email_input, password_input))
-    reset_password_text = ft.TextButton(text="Esqueci-me da Palavra-Passe", on_click=lambda _:reset_password_button_clicked)
-    registar_text = ft.TextButton(text="Registar", on_click=lambda _:register_button_clicked)
-
+    # Campo de senha com ícone de visibilidade dentro do TextField
+    password_input = ft.TextField(
+        label="Password",
+        password=True,
+        width=500,
+        height=50,
+        text_align="start",
+        suffix=ft.IconButton(
+            icon_size=20,
+            width=30,
+            height=30,
+            icon=ft.icons.VISIBILITY_OFF,
+            on_click=lambda e: toggle_password_visibility(password_input, e.control),
+            padding=ft.padding.all(2)
+        )
+    )
+    
+    # Botão de login
+    login_button = ft.ElevatedButton(
+        text="Login",
+        on_click=lambda _: login_button_clicked(email_input, password_input), 
+        height=60,
+        width=200,
+        style=ft.ButtonStyle(
+            text_style=ft.TextStyle(size=20)
+        )
+    )
+    
+    registar_text = ft.TextButton(
+        text="Registar", 
+        on_click=lambda _: register_button_clicked,        
+    )
+    
+    reset_password_text = ft.TextButton(
+        text="Esqueci-me da Palavra-Passe", 
+        on_click=lambda _: reset_password_button_clicked,
+    )
+    
+    img_login = ft.Image(
+        src=f"Logo/Superior/PNG/Logo.png",
+        fit=ft.ImageFit.FILL,
+        width=1200,
+        height=500,
+    )
+    
     login_container = ft.Container(
         content=ft.Column(
             [
-                ft.Text("Página de Login", size=30, weight=ft.FontWeight.BOLD),
-                #ft.Image(src="assets/Logo/Lateral/ICO/Logo.ico"),
+                img_login,
                 email_input,
                 password_input,
                 login_button,
@@ -26,17 +72,9 @@ def login_page(page: ft.Page):
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            expand=False
         ),
         alignment=ft.alignment.center,
-        padding=ft.Padding(left=20, right=20, top=20, bottom=20),
     )
 
-    page.add(
-        ft.Row(
-            [
-                login_container
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            expand=True 
-        )
-    )
+    page.add(login_container)
