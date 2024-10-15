d1284188b57b0a70a788a4f1690cff7896f22fc3
Bruno Oliveira <bruno.bx04@gmail.com>
2024-10-16 00:45:37 +0100

    [DEVELOP] - Remove static host and port

--- Code/API/API_MEDSTOCK/main.py
+++ Code/API/API_MEDSTOCK/main.py
@@ -16,4 +16,5 @@ api.add_middleware(
 api.include_router(GET_IS_ALIVE_router)
 
 if __name__ == "__main__":
-    uvicorn.run("main:api", host="localhost", port=5000, reload=True)
+    uvicorn.run("main:api", reload=True)
+    #uvicorn.run("main:api", host="localhost", port=5000, reload=True)
