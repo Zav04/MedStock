cd14dda7dabccdc621990ddcdfb45df7ff529fc0
Bruno Oliveira <bruno.bx04@gmail.com>
2024-11-06 01:43:00 +0000

    Revert "API"
    This reverts commit dc7b5d225c59026937e931920ad53a3f120d72fb.

--- Code/API/API_MEDSTOCK/Models/Item_Requerimento.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class C_RequerimentoMedicamentos(BaseModel):
-    item_id: Optional[int] = ''
-    requerimento_id: Optional[int] = ''
-    quantidade: Optional[int] = ''
--- Code/API/API_MEDSTOCK/main.py
+++ Code/API/API_MEDSTOCK/main.py
@@ -1,17 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
-from REQUESTS.CREATE_USER.POST_Create_User import router as POST_create_user_router
-from REQUESTS.CREATE_USER.PUT_Create_User import router as PUT_create_user_router
-from REQUESTS.CREATE_USER.GET_Create_User import router as GET_create_user_router
-from REQUESTS.CREATE_USER.DELETE_Create_User import router as DELETE_create_user_router
-from REQUESTS.ITEMS.GET_Items import router as GET_Items_router
-# from REQUESTS.ITEMS.PUT_Item import router as PUT_Item_router
-# from REQUESTS.ITEMS.DELETE_Item import router as DELETE_Item_router
 from REQUESTS.LOGIN.POST_Login import router as POST_login_router
 from REQUESTS.RESET_PASSWORD.POST_Reset_Password import router as POST_reset_password_router
+from REQUESTS.CREATE_USER.POST_Create_User import router as POST_create_user_router
 from REQUESTS.ROLES.GET_Roles import router as GET_roles_router
-
 api = FastAPI()
 
 api.add_middleware(
@@ -22,17 +15,12 @@ api.add_middleware(
     allow_headers=["*"],
 )
 
-api.include_router(POST_create_user_router)
-api.include_router(PUT_create_user_router)
-api.include_router(GET_create_user_router)
-api.include_router(DELETE_create_user_router)
+
+api.include_router(POST_login_router)
 api.include_router(POST_reset_password_router)
-api.include_router(GET_Items_router)
-# api.include_router(PUT_Item_router)
-# api.include_router(DELETE_Item_router)
+api.include_router(POST_create_user_router)
 api.include_router(GET_roles_router)
 
 if __name__ == "__main__":
-    # uvicorn.run("main:api", reload=True)
+    #uvicorn.run("main:api", reload=True)
     uvicorn.run("main:api", host="localhost", port=5000, reload=True)
-    
